119952f73ce247c065fb9bfd2fdbf7b8784c20d1
Hisashi Iwamoto <hisasi622@gmail.com>
2022-12-04 17:57:29 +0900

    ♻️ css split for components

--- components/Headline.js
+++ components/Headline.js
@@ -1,13 +1,13 @@
-import styles from '../styles/Home.module.css'
+import classes from './Headline.module.css'
 
 export function Headline(props) {
   return (
     <div>
-      <h1 className={styles.title}>
+      <h1 className={classes.title}>
         Welcome to <a href="https://nextjs.org">{ props.title }</a>
       </h1>
 
-      <p className={styles.description}>
+      <p className={classes.description}>
         Get started by editing {props.children}
       </p>
     </div>
--- components/Main.js
+++ components/Main.js
@@ -1,4 +1,3 @@
-import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import { Links } from './Links'
 import { Headline } from './Headline'
